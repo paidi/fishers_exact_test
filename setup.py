4258503b60e81bb6b62e788aa073e82b5e90fce5
Paidi Creed <paidi.creed@benevolent.ai>
2019-08-02 09:14:15 +0100

    Remove autogenerated file and update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ import sys
 from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.build_ext import build_ext
+from distutils.command.sdist import sdist
 
 version = '0.1.5'
 
+
 class _build_ext(build_ext):
     """build_ext command for use when numpy headers are needed.
-    
+
     https://stackoverflow.com/a/42163080/8083313
     """
+
     def run(self):
 
         # Import numpy here, only when headers are needed
@@ -20,31 +23,30 @@ class _build_ext(build_ext):
 
         # Call original build_ext command
         build_ext.run(self)
-        
 
 
 doc = open('README.rst').read()
 cfisher_ext = Extension('fisher.cfisher',
-                        ['src/cfisher.c'],
+                        ['src/cfisher.pyx'],
                         extra_compile_args=["-O3"])
 
 
 setup_options = dict(
-      name='fisher',
-      version=version,
-      description="Fast Fisher's Exact Test",
-      url = 'http://github.com/brentp/fishers_exact_test',
-      long_description=doc,
-      author="haibao tang, brent pedersen",
-      author_email="bpederse@gmail.com",
-      ext_modules=[ cfisher_ext ],
-      cmdclass = {'build_ext':_build_ext},
-#       install_requires=['numpy'],
-      setup_requires=['numpy'],
-      keywords='statistics cython',
-      license='BSD',
-      packages=['fisher'],
-      classifiers = [
+    name='fisher',
+    version=version,
+    description="Fast Fisher's Exact Test",
+    url='http://github.com/brentp/fishers_exact_test',
+    long_description=doc,
+    author="haibao tang, brent pedersen",
+    author_email="bpederse@gmail.com",
+    ext_modules=[cfisher_ext],
+    cmdclass={'build_ext': _build_ext, 'sdist': sdist},
+    #       install_requires=['numpy'],
+    setup_requires=['numpy'],
+    keywords='statistics cython',
+    license='BSD',
+    packages=['fisher'],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
@@ -54,7 +56,7 @@ setup_options = dict(
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3'
-      ],
+    ],
 )
 
 setup(**setup_options)
